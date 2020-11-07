7bb29c9ec0c641a2b52f2f2b16cf1949fafaa584
Emily Hosoya <emilyhosoya@gmail.com>
2020-11-06 17:11:22 -0700

    Fixed Results

--- src/Results.js
+++ src/Results.js
@@ -1,20 +1,24 @@
 import React from 'react'
+import './App.css'
 
 function Results(props){
     let pokemon = props.pokemon
-    
+    let abilities = pokemon.abilities.map(each =>(
+        <li>
+         {each.ability.name}       
+        </li>
+    ))
     // let abilities = pokemon.abilities.map(el => el.ability.name)
 
     console.log(pokemon.abilities)
 
     
     return(
-        <ul>
+        <ul class="list">
             <li>Name: {pokemon.name}</li>
             <li>ID: {pokemon.id}</li>
             <ul> Abilities:
-                {pokemon.abilities.forEach(el => {
-                <li>{el.ability.name}</li>})}
+                {abilities}
             </ul>
         </ul>
     )
